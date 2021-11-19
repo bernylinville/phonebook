30fd583b9ecd1c03fa63a77c8397fcf17c56c508
Berny Linville <kylechou@devopsthink.org>
2021-11-19 17:17:25 +0800

    Update main.go

--- main.go
+++ main.go
@@ -4,7 +4,7 @@ Copyright © 2021 Mihalis Tsoukalos <mihalistsoukalos@gmail.com>
 */
 package main
 
-import "github.com/mactsouk/phonebook/cmd"
+import "github.com/bernylinville/phonebook/cmd"
 
 func main() {
 	cmd.Execute()
